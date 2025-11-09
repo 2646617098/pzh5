c267ffa2ca9ee0d54bf97d1c2741b9463d78b7e7
981928 <2646617098@qq.com>
2025-11-09 13:27:52 +0800

    34feat(Main.vue): 添加底部导航栏并实现路由跳转
    - 引入 van-tabbar 和 van-tabbar-item 组件
    - 使用 v-for 遍历路由配置生成导航项
    - 动态绑定图标和名称到导航项
    - 实现点击导航项跳转对应路由
    - 添加 onMounted 钩子计算当前激活路由
    - 引入 vue-router 的 useRouter 和 useRoute 方法

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,7 +1,22 @@
 <template>
-    111
+    <RouterView />
+    <van-tabbar v-model="active">
+        <!-- :url="`#/${item.path}`"用于跳转 -->
+        <van-tabbar-item v-for="item in router.options.routes[0].children" :key="item.path" :icon="item.meta.icon"
+            :url="`#/${item.path}`">{{ item.meta.name }}</van-tabbar-item>
+    </van-tabbar>
 </template>
 <script setup>
+import { ref, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 
+const active = ref(0)
+const router = useRouter()
+const route = useRoute()
+onMounted(() => {
+    // console.log(router, "router")
+    const data = router.options.routes[0]
+    active.value = data.children.findIndex(item => '/' + item.path === route.path)
+})
 </script>
 <style scoped></style>

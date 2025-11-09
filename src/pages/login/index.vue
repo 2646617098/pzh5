4dd68a1d8319705fdf4c1b985ab436fdb7bc7263
981928 <2646617098@qq.com>
2025-11-09 13:43:59 +0800

    35feat(login): 添加用户登录表单页面
    - 引入 Vant 组件库的表单组件构建登录界面
    - 实现用户名和密码输入框及提交按钮
    - 添加表单验证规则确保必填字段
    - 移除默认样式文件引用，使用 Less 编写局部样式
    - 设置标题居中和按钮外边距样式

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,7 +1,36 @@
 <template>
-
+    <h1>用户登录</h1>
+    <van-form @submit="onSubmit">
+        <van-cell-group inset>
+            <van-field v-model="userName" name="用户名" label="用户名" placeholder="用户名"
+                :rules="[{ required: true, message: '请填写用户名' }]" />
+            <van-field v-model="passWord" type="password" name="密码" label="密码" placeholder="请输入密码"
+                :rules="[{ required: true, message: '请填写密码' }]" />
+        </van-cell-group>
+    </van-form>
+    <div>
+        <van-button class="btn" type="primary" round block native-type="submit">提交</van-button>
+    </div>
 </template>
 <script setup>
+import { reactive } from 'vue';
 
+// 表单数据
+const form = reactive({
+    userName: '',
+    passWord: ''
+})
+// 表达引入
+const onSubmit = () => {
+
+}
 </script>
-<style scoped></style>
+<style lang="less" scoped>
+h1 {
+    text-align: center;
+}
+
+.btn {
+    margin: 16px;
+}
+</style>
